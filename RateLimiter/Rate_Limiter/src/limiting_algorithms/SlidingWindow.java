855c61c7972b6611b4716af44b8d961bbc771fe6
Swapnil Jain <swapniljain240@gmail.com>
2024-02-29 00:12:43 +0530

    sliding window algorithm

--- RateLimiter/Rate_Limiter/src/limiting_algorithms/SlidingWindow.java
+++ RateLimiter/Rate_Limiter/src/limiting_algorithms/SlidingWindow.java
@@ -1,27 +1,50 @@
 package limiting_algorithms;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.time.Instant;
+import java.util.HashMap;
+
+import java.util.Map;
+
 
 //6 req per second window
 public class SlidingWindow implements Limiter{
 
-    private Queue<Long> window;
+
+    Map<Long,Long> window;
 
     private int maxTime;
 
     private int capacity;
 
+    private long mapSize;
+
     public SlidingWindow(){
-        window= new LinkedList();
+        window= new HashMap<>();
+        window.put(Instant.now().toEpochMilli(), 0L);
         capacity=6;
         maxTime = 10;
+        mapSize=1;
     }
 
 
 
     @Override
     public Boolean grantAccess(String ipAddress) {
-        return null;
+
+        Long currTime = Instant.now().toEpochMilli();
+        Long startWindow = currTime-maxTime;
+
+        window.put(currTime,mapSize);
+        mapSize++;
+
+        if(window.containsKey(startWindow)){
+            long reqCount = mapSize-window.get(startWindow);
+
+            return reqCount<capacity ? true : false;
+        }
+
+        return true;
+
+
     }
 }
